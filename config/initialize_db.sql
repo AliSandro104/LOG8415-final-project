056f6b844f342afdce2fa0b38748a9d4b31f39f4
AliSandro104 <alisandro2000@hotmail.com>
2023-12-05 22:24:18 -0500

    remove generated files

--- config/initialize_db.sql
+++
@@ -1,3 +0,0 @@
-SOURCE /tmp/sakila-db/sakila-schema.sql;SOURCE /tmp/sakila-db/sakila-data.sql;USE sakila;GRANT ALL ON *.* TO 'worker1'@'ip-172-31-42-18.ec2.internal' IDENTIFIED BY 'worker1';
-GRANT ALL ON *.* TO 'worker2'@'ip-172-31-42-18.ec2.internal' IDENTIFIED BY 'worker2';
-GRANT ALL ON *.* TO 'worker3'@'ip-172-31-42-18.ec2.internal' IDENTIFIED BY 'worker3';
